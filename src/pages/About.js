81f3d955bb4ac33a39f1d20014d38e04a23c4cbe
Tyler <tyler.audino@ufl.edu>
2024-07-05 09:53:18 -0400

    before changing to smooth scroll

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import "../styles/About.css"
 import BandPic from "../assets/UF_Band.png"
+import pythonLogo from "../assets/Python-logo.png"
 
 function About() {
   return (
     <div className='about'>
       <div id='container'>
         <div className='ufPhoto'>
-          <img src={BandPic}/>
+          <img src={BandPic} alt="Tyler in a Gator Marching Band uniform, playing mellophone on the UF football field at halftime."/>
         </div>
         <div id="textBoxRight">
           <h1>Education</h1>
-          <p>Currently pursuing a B.S. in Computer Science with a minor in Statistics at the University of Florida</p>
+          <p>I'm currently pursuing a B.S. in Computer Science with a minor in Statistics at the University of Florida!</p>
           <ul> 
             <li>Expected May 2026</li>
             <li>GPA: 3.88</li>
@@ -36,12 +37,23 @@ function About() {
         <div id="textBoxLeft">
           <h1>Skills</h1>
           <h3>Technical:</h3>
-          <p></p>
+          <ul>
+            <li>Python (NumPy, Pandas, Plot.ly)</li>
+            <li>Javascript (React.js, Node.js)</li>
+            <li>C++/C (SFML)</li>
+            <li>HTML/CSS</li>
+            <li>Git</li>
+          </ul>
           <h3>Non-Technical:</h3>
-          <p></p>
+          <ul>
+            <li>AGILE Framework</li>
+            <li>Kanban</li>
+            <li>Communication</li>
+            <li>Problem Solving</li>
+          </ul>
         </div>
         <div className='skillsImages'>
-          <p>Images here</p>
+          <img src={pythonLogo} alt="The Python programming language logo" id="python"/>
         </div>
       </div>
 
@@ -52,6 +64,11 @@ function About() {
 
         <div id="textBoxRight"> 
         <h1>Fun Facts!</h1>
+        <ul>
+          <li>I've played the French horn for 8 years and the mellophone for 6 years!</li>
+          <li>I founded <a href='https://www.depop.com/tyxplr/' target="_blank" rel="noopener noreferrer">my own buisness</a> at 15, selling upcycled and gently-loved clothing online.</li>
+          <li>I've taught myself how to crochet and sew as a hobby!</li>
+        </ul>
         </div>
       </div>
     </div>
