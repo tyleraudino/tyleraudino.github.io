a560553026be897753c5a274323b7c42345caa79
Tyler <tyler.audino@ufl.edu>
2024-07-10 13:42:26 -0400

    finished experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -13,15 +13,21 @@ function Experience() {
           <div className='container'>       
                 <h3>Lanuages</h3>
                   <p>Python - Experienced - 2 years</p>
+                  <hr/>
                   <p>C++/C - Intermediate - 1 year </p>
+                  <hr/>
                   <p>Javascript/React.js - Intermediate - 6 months</p>
+                  <hr/>
                   <p>HTML/CSS - Intermediate - 1 year</p>
           </div>
           <div className='container'>
                 <h3>Other</h3>
                 <p>Git</p>
+                <hr/>
                 <p>AGILE Framework</p>
+                <hr/>
                 <p>Kanban</p>
+                <hr/>
                 <p>Research Computing</p>
             </div>
         </div>
@@ -29,12 +35,21 @@ function Experience() {
         
       <div className='halfPage'>
         <div className='largeContainer dark' id="workexp">
-          <div className='header'>
-            <h3>Work Experience</h3>
-          </div>
           <div className='leftText'>
-            <p>Florida Blue (May 2024 - Present)</p>
-            <p>Application Development Intern</p>
+            <p><b>Application Development Intern</b> - (May 2024 - Present)</p>
+            <p>Florida Blue </p>
+            <br/>
+            <p><b>Research Assistant</b> - (September 2023 - Present)</p>
+            <p>UF INDIE Lab </p>
+            <br/>
+            <p><b>Webmaster</b> - (January 2024 - Present)</p>
+            <p>Kappa Kappa Psi - Alpha Eta Chapter </p>
+            <br/>
+            <p><b>Data Analyst Intern</b> - (January 2024 - May 2024)</p>
+            <p>UF Active Learning Program </p>
+            <br/>
+            <p><b>Front Desk Assistant</b> - (August 2022 - May 2024)</p>
+            <p>UF Student Activities and Involvement</p>
           </div>
           <div className='rightText'>
             <p></p>
