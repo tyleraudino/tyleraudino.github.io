60d88bdae96da90c607ecbf9b3447ed24d41175e
Tyler <tyler.audino@ufl.edu>
2025-02-19 12:24:34 -0500

    create form file

---
+++ src/components/ContactForm.js
@@ -0,0 +1,42 @@
+// Make sure to run npm install @formspree/react
+// For more help visit https://formspr.ee/react-help
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xwpeprnr");
+  if (state.succeeded) {
+      return <p>Thanks for reaching out! I will respond to you as soon as possible via the email submitted.</p>;
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default ContactForm
